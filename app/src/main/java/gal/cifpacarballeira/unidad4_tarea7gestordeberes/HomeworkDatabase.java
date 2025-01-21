b0f9453689896bcb505342fdf2896154e3c68d4c
miguel <migonrod@gmail.com>
2025-01-21 11:21:01 +0100

    Añadida rama para trabajar con SQLite

--- app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkDatabase.java
+++ app/src/main/java/gal/cifpacarballeira/unidad4_tarea7gestordeberes/HomeworkDatabase.java
@@ -28,12 +28,7 @@ public class HomeworkDatabase extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    /**
-     * Called when the database is created for the first time. This is where the
-     * creation of tables and the initial population of the tables should happen.
-     *
-     * @param db The database.
-     */
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         // Crear la tabla de deberes
@@ -47,39 +42,6 @@ public class HomeworkDatabase extends SQLiteOpenHelper {
 
     }
 
-    /**
-     * Called when the database needs to be upgraded. The implementation
-     * should use this method to drop tables, add tables, or do anything else it
-     * needs to upgrade to the new schema version.
-     *
-     * <p>
-     * The SQLite ALTER TABLE documentation can be found
-     * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
-     * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
-     * you can use ALTER TABLE to rename the old table, then create the new table and then
-     * populate the new table with the contents of the old table.
-     * </p><p>
-     * This method executes within a transaction.  If an exception is thrown, all changes
-     * will automatically be rolled back.
-     * </p>
-     * <p>
-     * <em>Important:</em> You should NOT modify an existing migration step from version X to X+1
-     * once a build has been released containing that migration step.  If a migration step has an
-     * error and it runs on a device, the step will NOT re-run itself in the future if a fix is made
-     * to the migration step.</p>
-     * <p>For example, suppose a migration step renames a database column from {@code foo} to
-     * {@code bar} when the name should have been {@code baz}.  If that migration step is released
-     * in a build and runs on a user's device, the column will be renamed to {@code bar}.  If the
-     * developer subsequently edits this same migration step to change the name to {@code baz} as
-     * intended, the user devices which have already run this step will still have the name
-     * {@code bar}.  Instead, a NEW migration step should be created to correct the error and rename
-     * {@code bar} to {@code baz}, ensuring the error is corrected on devices which have already run
-     * the migration step with the error.</p>
-     *
-     * @param db         The database.
-     * @param oldVersion The old database version.
-     * @param newVersion The new database version.
-     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Actualizar la base de datos (si cambia la versión)
